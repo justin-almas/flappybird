33c7cda9cd695e50e72d4edf6fcb0e604c5e2588
Justin Almas <justinaalmas@gmail.com>
2023-08-05 10:45:13 -0400

    add basic movement

--- main.cpp
+++ main.cpp
@@ -17,15 +17,30 @@ int main()
     sprite.setTexture(bird);
     sprite.setScale(0.1,0.1);
     sprite.setPosition(400, 300);
+    sf::Vector2f velocity (0.0f, 0.0f);
+    const float gravity = 0.000003f;
+    const float changeY = -0.03f;
+    bool isJumping = false;
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            if (event.type == sf::Event::Closed) {
                 window.close();
+            }
+            else if (event.type == sf::Event::KeyPressed && sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+                if (!isJumping) {
+                    velocity.y = changeY;
+                    isJumping = true;
+                }
+            }
+            else if (event.type == sf::Event::KeyReleased && event.key.code == sf::Keyboard::W) {
+                isJumping = false;
+            }
         }
-
+        sprite.move(velocity);
+        velocity.y += gravity;
         window.clear();
         window.draw(sprite);
         window.display();
